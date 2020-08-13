1a75af377ac096f3b8831620877afcf7c22db2eb
brookeperkins <brookemmperkins@gmail.com>
2020-08-13 15:00:22 -0700

    URL

--- src/queens-api.js
+++ src/queens-api.js
@@ -1,6 +1,6 @@
 import request from 'superagent';
 
-const URL = 'http://localhost:3000';
+const URL = 'https://back-end-lab-08.herokuapp.com';
 
 export function fetchThoseQueens() {
     return request.get(`${URL}/queens`);
