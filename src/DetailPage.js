78a81ca0f2054a592e78a8e9e78d400a18b6eeff
brookeperkins <brookemmperkins@gmail.com>
2020-08-18 00:31:11 -0700

    detailspage

--- src/DetailPage.js
+++ src/DetailPage.js
@@ -17,10 +17,14 @@ class DetailPage extends Component {
     
     render() {
         return (
-            <div className="detail">
+            <main>
             <h2>All hail {this.state.queen.name}!</h2>
-            As she always says: {this.state.queen.quote}!
-            </div>
+            <img alt={this.state.queen.image_url} src={this.state.queen.image_url} />
+            <h2>What does she win?</h2>
+            <p>{this.state.queen.winner}</p>
+            <h2>As she always says:</h2> 
+            <p>{this.state.queen.quote}!</p>
+            </main>
         )
     }
 }
